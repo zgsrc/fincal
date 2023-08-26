ee412cfc8107c148948bf263581692e8a0c1ac33
Eruditis <inc@eruditis.com>
2023-08-26 14:17:09 -0500

    aider: Added unit tests for the functions in venue.py.
    # Aider chat conversation:
    
    USER: I want to make sure everything is working as expected. Can you add unit tests to the project to validate all the functions of venue.py?
    
    ASSISTANT: Sure, we can add a new file `test_venue.py` in a `tests` directory to hold our unit tests. We'll use the `unittest` module from Python's standard library to create our tests.
    
    Here's the edit block for the new `test_venue.py` file:
    
    ```python
    tests/test_venue.py
    <<<<<<< HEAD
    =======
    import unittest
    from exchanges.venue import Venue
    import pandas as pd
    
    class TestVenue(unittest.TestCase):
        def setUp(self):
            self.venue = Venue('exchanges/nyse.yml')
    
        def test_is_trading_time(self):
            timestamp = pd.Timestamp('2022-01-03 10:00:00')  # A Monday within trading hours
            self.assertTrue(self.venue.is_trading_time(timestamp))
    
        def test_is_regular_trading_day(self):
            day = pd.Timestamp('2022-01-03')  # A Monday
            self.assertTrue(self.venue.is_regular_trading_day(day))
    
        def test_is_non_trading_day(self):
            day = pd.Timestamp('2022-01-01')  # A Saturday
            self.assertTrue(self.venue.is_non_trading_day(day))
    
        def test_is_regular_non_trading_day(self):
            day = pd.Timestamp('2022-01-01')  # A Saturday
            self.assertTrue(self.venue.is_regular_non_trading_day(day))
    
        def test_is_irregular_non_trading_day(self):
            day = pd.Timestamp('2022-01-01')  # A Saturday
            self.assertFalse(self.venue.is_irregular_non_trading_day(day))
    
        def test_is_partial_trading_day(self):
            day = pd.Timestamp('2022-01-01')  # A Saturday
            self.assertFalse(self.venue.is_partial_trading_day(day))
    
        def test_get_trading_hours(self):
            day = pd.Timestamp('2022-01-03')  # A Monday
            trading_hours, reason = self.venue.get_trading_hours(day)
            self.assertEqual(reason, "Regular Trading Day")
    
    if __name__ == '__main__':
        unittest.main()
    >>>>>>> updated
    ```
    
    This file contains a `TestVenue` class that inherits from `unittest.TestCase`. The `setUp` method is run before each test method, and it sets up a `Venue` instance that the test methods can use.
    
    Each test method in the `TestVenue` class tests a different method of the `Venue` class. The `assertTrue` and `assertFalse` methods from `unittest.TestCase` are used to assert that the methods of the `Venue` class return the expected results.
    
    The `if __name__ == '__main__': unittest.main()` line at the end of the file allows the tests to be run by executing the `test_venue.py` file directly.
    
    Please note that these tests are basic and may need to be expanded to cover more edge cases and more complex scenarios.

---
+++ tests/test_venue.py
@@ -0,0 +1,39 @@
+import unittest
+from exchanges.venue import Venue
+import pandas as pd
+
+class TestVenue(unittest.TestCase):
+    def setUp(self):
+        self.venue = Venue('exchanges/nyse.yml')
+
+    def test_is_trading_time(self):
+        timestamp = pd.Timestamp('2022-01-03 10:00:00')  # A Monday within trading hours
+        self.assertTrue(self.venue.is_trading_time(timestamp))
+
+    def test_is_regular_trading_day(self):
+        day = pd.Timestamp('2022-01-03')  # A Monday
+        self.assertTrue(self.venue.is_regular_trading_day(day))
+
+    def test_is_non_trading_day(self):
+        day = pd.Timestamp('2022-01-01')  # A Saturday
+        self.assertTrue(self.venue.is_non_trading_day(day))
+
+    def test_is_regular_non_trading_day(self):
+        day = pd.Timestamp('2022-01-01')  # A Saturday
+        self.assertTrue(self.venue.is_regular_non_trading_day(day))
+
+    def test_is_irregular_non_trading_day(self):
+        day = pd.Timestamp('2022-01-01')  # A Saturday
+        self.assertFalse(self.venue.is_irregular_non_trading_day(day))
+
+    def test_is_partial_trading_day(self):
+        day = pd.Timestamp('2022-01-01')  # A Saturday
+        self.assertFalse(self.venue.is_partial_trading_day(day))
+
+    def test_get_trading_hours(self):
+        day = pd.Timestamp('2022-01-03')  # A Monday
+        trading_hours, reason = self.venue.get_trading_hours(day)
+        self.assertEqual(reason, "Regular Trading Day")
+
+if __name__ == '__main__':
+    unittest.main()
