0bffe3cd38779e47c9499f5b077891b43c4cbcc8
Eruditis <inc@eruditis.com>
2023-08-26 18:09:19 -0500

    aider: Reverted `test_venue.py` file back to the default unittest format.
    # Aider chat conversation:
    
    ASSISTANT: To revert the `test_venue.py` file back to the default unittest format, we need to modify the `tests\test_venue.py` file.
    
    Here are the changes:
    
    1. Import the `unittest` module instead of `pytest`.
    2. Create a `TestVenue` class that inherits from `unittest.TestCase`.
    3. Convert each test function into a method of the `TestVenue` class.
    4. Replace the `assert` statements with the appropriate `self.assert*` methods from the `unittest.TestCase` class.
    5. Remove the `venue` fixture and instead create a `setUp` method in the `TestVenue` class that initializes the `Venue` instance before each test.
    
    Here is the *edit block* for these changes:
    
    ```python
    tests\test_venue.py
    <<<<<<< HEAD
    import pytest
    from exchanges.venue import Venue
    import pandas as pd
    
    @pytest.fixture
    def venue():
        return Venue('../exchanges/nyse.yml')
    
    def test_is_trading_time(venue):
        timestamp = pd.Timestamp('2022-01-03 10:00:00')  # A Monday within trading hours
        assert venue.is_trading_time(timestamp)
    
    def test_is_regular_trading_day(venue):
        day = pd.Timestamp('2022-01-03')  # A Monday
        assert venue.is_regular_trading_day(day)
    
    def test_is_non_trading_day(venue):
        day = pd.Timestamp('2022-01-01')  # A Saturday
        assert venue.is_non_trading_day(day)
    
    def test_is_regular_non_trading_day(venue):
        day = pd.Timestamp('2022-01-01')  # A Saturday
        assert venue.is_regular_non_trading_day(day)
    
    def test_is_irregular_non_trading_day(venue):
        day = pd.Timestamp('2022-01-01')  # A Saturday
        assert not venue.is_irregular_non_trading_day(day)
    
    def test_is_partial_trading_day(venue):
        day = pd.Timestamp('2022-01-01')  # A Saturday
        assert not venue.is_partial_trading_day(day)
    
    def test_get_trading_hours(venue):
        day = pd.Timestamp('2022-01-03')  # A Monday
        trading_hours, reason = venue.get_trading_hours(day)
        assert reason == "Regular Trading Day"
    =======
    import unittest
    from exchanges.venue import Venue
    import pandas as pd
    
    class TestVenue(unittest.TestCase):
        def setUp(self):
            self.venue = Venue('../exchanges/nyse.yml')
    
        def test_is_trading_time(self):
            timestamp = pd.Timestamp('2022-01-03 10:00:00')  # A Monday within trading hours
            self.assertTrue(self.venue.is_trading_time(timestamp))
    
        def test_is_regular_trading_day(self):
            day = pd.Timestamp('2022-01-03')  # A Monday
            self.assertTrue(self.venue.is_regular_trading_day(day))
    
        def test_is_non_trading_day(self):
            day = pd.Timestamp('2022-01-01')  # A Saturday
            self.assertTrue(self.venue.is_non_trading_day(day))
    
        def test_is_regular_non_trading_day(self):
            day = pd.Timestamp('2022-01-01')  # A Saturday
            self.assertTrue(self.venue.is_regular_non_trading_day(day))
    
        def test_is_irregular_non_trading_day(self):
            day = pd.Timestamp('2022-01-01')  # A Saturday
            self.assertFalse(self.venue.is_irregular_non_trading_day(day))
    
        def test_is_partial_trading_day(self):
            day = pd.Timestamp('2022-01-01')  # A Saturday
            self.assertFalse(self.venue.is_partial_trading_day(day))
    
        def test_get_trading_hours(self):
            day = pd.Timestamp('2022-01-03')  # A Monday
            trading_hours, reason = self.venue.get_trading_hours(day)
            self.assertEqual(reason, "Regular Trading Day")
    
    if __name__ == '__main__':
        unittest.main()
    >>>>>>> updated
    ```

--- tests/test_venue.py
+++ tests/test_venue.py
@@ -1,36 +1,39 @@
-import pytest
+import unittest
 from exchanges.venue import Venue
 import pandas as pd
 
-@pytest.fixture
-def venue():
-    return Venue('../exchanges/nyse.yml')
+class TestVenue(unittest.TestCase):
+    def setUp(self):
+        self.venue = Venue('../exchanges/nyse.yml')
 
-def test_is_trading_time(venue):
-    timestamp = pd.Timestamp('2022-01-03 10:00:00')  # A Monday within trading hours
-    assert venue.is_trading_time(timestamp)
+    def test_is_trading_time(self):
+        timestamp = pd.Timestamp('2022-01-03 10:00:00')  # A Monday within trading hours
+        self.assertTrue(self.venue.is_trading_time(timestamp))
 
-def test_is_regular_trading_day(venue):
-    day = pd.Timestamp('2022-01-03')  # A Monday
-    assert venue.is_regular_trading_day(day)
+    def test_is_regular_trading_day(self):
+        day = pd.Timestamp('2022-01-03')  # A Monday
+        self.assertTrue(self.venue.is_regular_trading_day(day))
 
-def test_is_non_trading_day(venue):
-    day = pd.Timestamp('2022-01-01')  # A Saturday
-    assert venue.is_non_trading_day(day)
+    def test_is_non_trading_day(self):
+        day = pd.Timestamp('2022-01-01')  # A Saturday
+        self.assertTrue(self.venue.is_non_trading_day(day))
 
-def test_is_regular_non_trading_day(venue):
-    day = pd.Timestamp('2022-01-01')  # A Saturday
-    assert venue.is_regular_non_trading_day(day)
+    def test_is_regular_non_trading_day(self):
+        day = pd.Timestamp('2022-01-01')  # A Saturday
+        self.assertTrue(self.venue.is_regular_non_trading_day(day))
 
-def test_is_irregular_non_trading_day(venue):
-    day = pd.Timestamp('2022-01-01')  # A Saturday
-    assert not venue.is_irregular_non_trading_day(day)
+    def test_is_irregular_non_trading_day(self):
+        day = pd.Timestamp('2022-01-01')  # A Saturday
+        self.assertFalse(self.venue.is_irregular_non_trading_day(day))
 
-def test_is_partial_trading_day(venue):
-    day = pd.Timestamp('2022-01-01')  # A Saturday
-    assert not venue.is_partial_trading_day(day)
+    def test_is_partial_trading_day(self):
+        day = pd.Timestamp('2022-01-01')  # A Saturday
+        self.assertFalse(self.venue.is_partial_trading_day(day))
 
-def test_get_trading_hours(venue):
-    day = pd.Timestamp('2022-01-03')  # A Monday
-    trading_hours, reason = venue.get_trading_hours(day)
-    assert reason == "Regular Trading Day"
+    def test_get_trading_hours(self):
+        day = pd.Timestamp('2022-01-03')  # A Monday
+        trading_hours, reason = self.venue.get_trading_hours(day)
+        self.assertEqual(reason, "Regular Trading Day")
+
+if __name__ == '__main__':
+    unittest.main()
